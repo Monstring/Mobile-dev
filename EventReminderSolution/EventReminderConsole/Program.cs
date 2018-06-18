f717d52893cf3c6773b3b78715aa98d3090b55f3
Monstring <monstring1@hotmail.com>
2018-06-18 19:21:35 +0300

    hooked up Add command and List command with the list

--- EventReminderSolution/EventReminderConsole/Program.cs
+++ EventReminderSolution/EventReminderConsole/Program.cs
@@ -21,26 +21,30 @@ namespace EventReminderConsole
             //}
             //kld code end 
 
-            List<string> list = new List<string>();
+            List<string> eventList = new List<string>();
 
 
             while (true)
             {
 
-                Console.WriteLine("\nEnter a command");
+                Console.Write("\n>");
                 string input = Console.ReadLine();
-                Console.WriteLine("You've selected "+ input);
 
                 string[] tokens = input.Split(' ');
 
                 string command = tokens[0].ToLower();
                 if (command.StartsWith("add"))
                 {
-                    Console.WriteLine($"you've added {tokens[1]} on {tokens[2]}");                    
+                    Console.WriteLine($"you've added {tokens[1]} on {tokens[2]}");
+                    eventList.Add(string.Format("{0} {1}", tokens[1], tokens[2]));
                 }
                 else if (command == "list")
                 {
-                    Console.WriteLine("Bringing up the list");
+                    for (int i = 0; i < eventList.Count; i++)
+                    {
+                        Console.WriteLine((i+1)+"-"+eventList[i]);
+
+                    }
                 }
                 else if (command == "exit")
                 {
