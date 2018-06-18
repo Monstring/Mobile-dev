09f64537ba0c462a1b3ea77a2ed08dbd52838e14
Monstring <monstring1@hotmail.com>
2018-06-18 18:40:18 +0300

    -Added a loop to the program and an exit command

--- EventReminderSolution/EventReminderConsole/Program.cs
+++ EventReminderSolution/EventReminderConsole/Program.cs
@@ -23,26 +23,33 @@ namespace EventReminderConsole
 
             List<string> list = new List<string>();
 
-            
 
-            Console.WriteLine("Enter a command");
-            string input = Console.ReadLine();
-            Console.WriteLine("You've selected "+ input);
+            while (true)
+            {
 
-            string token = input.ToLower();
-            if (token == "add")
-            {
-                Console.WriteLine("Adding a " + token);
-            }
-            else if (token == "list")
-            {
-                Console.WriteLine("Bringing up the list");
-            }
-            else
-            {
-                Console.WriteLine("learn to type");
-            }
+                Console.WriteLine("\nEnter a command");
+                string input = Console.ReadLine();
+                Console.WriteLine("You've selected "+ input);
 
+                string token = input.ToLower();
+                if (token == "add")
+                {
+                    Console.WriteLine("Adding a " + token);
+                }
+                else if (token == "list")
+                {
+                    Console.WriteLine("Bringing up the list");
+                }
+                else if (token == "exit")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("learn to type");
+                }
+            }
+            Console.WriteLine("Closing program");
             Console.ReadLine();
         }
     }
