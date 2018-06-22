221cfd4ca9f17eb9f2e8dcc0ec8adfbf5b4d6ede
Monstring <monstring1@hotmail.com>
2018-06-22 19:40:30 +0300

    Incapsulated 'add' and 'list' actions

--- EventReminderSolution/EventReminderConsole/Program.cs
+++ EventReminderSolution/EventReminderConsole/Program.cs
@@ -7,32 +7,21 @@ namespace EventReminderConsole
     {
         static void Main(string[] args)
         {
-            List<Reminder> eventList = new List<Reminder>();
             while (true)
             {
                 Console.Write("\n>");
                 string input = Console.ReadLine();
-
                 string[] tokens = input.Split(' ');
-
                 string command = tokens[0].ToLower();
+
                 if (command.StartsWith("add"))
                 {
                     Console.WriteLine($"you've added {tokens[1]} on {tokens[2]}");
-
-                    Reminder newEvent = new Reminder(tokens[1], tokens[2]);
-                        
-                    eventList.Add(newEvent);
+                    AddReminder(tokens[1], tokens[2]);
                 }
                 else if (command == "list")
                 {
-                    for (int i = 0; i < eventList.Count; i++)
-                    {
-                        Reminder e = eventList[i];
-                       
-                        Console.WriteLine((i+1) + "-" + e.ToString());
-
-                    }
+                    ListReminders();
                 }
                 else if (command == "exit")
                 {
@@ -45,6 +34,19 @@ namespace EventReminderConsole
             }
             Console.WriteLine("Closing program");
             Console.ReadLine();
+        }
+        static List<Reminder> eventList = new List<Reminder>();
+        private static void AddReminder(string name, string date) {
+            Reminder newEvent = new Reminder(name, date);
+            eventList.Add(newEvent);
+        }
+
+        private static void ListReminders() {
+            for (int i = 0; i < eventList.Count; i++)
+            {
+                Reminder e = eventList[i];
+                Console.WriteLine((i + 1) + "-" + e.ToString());
+            }
         }
     }
 
